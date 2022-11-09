448bcb0932d830f57dd3a42e482fcb4320eb32e0
YuvalBrdsk <yuval.brdsk@gmail.com>
2022-11-09 12:37:44 +0200

    NameChanges

--- LogoSection_Class.py
+++ LogoSection_Class.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-class HomePage:
+class LogoSection:
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
--- OrderPayment_Class.py
+++ OrderPayment_Class.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-class NewAccount:
+class OrderPayment:
 
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
