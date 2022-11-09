073b2e9d121ada6d0d99aa248c2490da0e344a70
YuvalBrdsk <yuval.brdsk@gmail.com>
2022-11-09 11:22:10 +0200

    Classes

---
+++ AutomationProject/LogoSection_Class.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class HomePage:
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
+
