f28524c85162506c4f1cce075e551f756b3f9524
YuvalBrdsk <111422208+YuvalBrdsk@users.noreply.github.com>
2022-11-09 10:32:33 +0200

    Delete Lesson_6 directory

--- Lesson_6/exercises_Michal/Dictionary/7.5.py
+++
@@ -1,6 +0,0 @@
-dic1 = {1:10,2:20,3:30,4:40,5:50}
-
-k = sum(dic1.keys())
-v = sum(dic1.values())
-print("k",k,"v",v)
-
--- Lesson_6/exercises_Michal/Dictionary/7.6.py
+++
@@ -1,6 +0,0 @@
-dic1 = {1:10,2:20,3:30,4:40,5:50}
-
-num = int(input("Enter a number: "))
-
-del dic1[num]
-print(dic1)
